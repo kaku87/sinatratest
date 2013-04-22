eeaa14176085efcf33097c01e20f79fe1b2d2a98
kaku <ghjcumt2008@gmail.com>
2013-04-22 18:06:54 +0900

    sinatra test

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'haml'
+gem 'sequel'
+gem 'sqlite3'
+gem 'sanitize'
+
+group :develepment do
+  gem 'sinatra-contrib'
+end
---
+++ server.rb
@@ -0,0 +1,63 @@
+if development?
+  require 'sinatra/reloader'
+end
+
+Sequel::Model.plugin(:schema)
+
+Sequel.sqlite('db/kaku.db')
+
+class Entries < Sequel::Model
+  plugin :timestamps, :update_on_create => true
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      string :url
+      timestamp :created_at
+      timestamp :updated_at
+    end
+    create_table
+  end
+end
+
+get '/' do
+  @title = 'My Title'
+  @entries = Entries.all
+  haml :index
+end
+
+post '/add' do
+  @url= params[:url]
+  #Entries.insert :url => @url
+  @entry = Entries.new :url => Sanitize.clean(params[:url])
+  @entry.save
+  redirect '/'
+end
+
+helpers do 
+def add_schema(url)
+    if url !~ /^https?/
+      if url =~ /\/\//
+        "http:#{url}"
+      else
+        "http://#{url}"
+      end
+    else
+      url
+    end
+  end
+
+  def shorten(str, max = 20)
+    if str.length > max
+      "#{str[0..max.to_i]}..."
+    else
+      str
+    end
+  end
+  def timefmt(time)
+    if time
+      time.strftime("%y-%m-%d %H:%M:%S")
+    else
+      ''
+    end
+  end
+end
